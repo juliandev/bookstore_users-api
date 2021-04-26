39e1591bfa16bdd31181159d7957f73e71cfb1fd
Julian Castañeda <jccastanedaal@gmail.com>
2021-04-26 23:00:40 +0000

    CreateUser in controller was partially implemented

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -3,10 +3,24 @@ package users
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"github.com/juliandev/bookstore_users-api/domain/users"
+	"fmt"
+	"github.com/juliandev/bookstore_users-api/services"
 )
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implemented me!")
+	var user users.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		//TODO: Handle json error
+		return
+	}
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		//TODO: Handle error
+		return
+	}
+	fmt.Println(user)
+	c.JSON(http.StatusCreated, result)
 }
 
 func GetUser(c *gin.Context) {
--- services/users_service.go
+++ services/users_service.go
@@ -1,1 +1,9 @@
 package services
+
+import (
+	"github.com/juliandev/bookstore_users-api/domain/users"
+)
+
+func CreateUser(user users.User) (*users.User, error) {
+	return &user, nil
+}
